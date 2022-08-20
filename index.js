a2ac88b19197c6fd373b781759cf465de327b8bf
Andrew Wang <Andrewruujin@gmail.com>
2022-08-20 19:29:54 -0400

    commit tree a7972d33dc880d51052c61e07519e92c01d493fe

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(funActivity = "roller-skate"){
+    return `This Saturday, I want to ${funActivity}!`;
+}
+function mondayWork(workActivity = 'go to the office'){
+    return `This Monday, I will ${workActivity}.`
+}
+function wrapAdjective(wrapper){
+    return function (phrase){
+        return `You are ${wrapper}${phrase}${wrapper}!`;
+    };
+}
